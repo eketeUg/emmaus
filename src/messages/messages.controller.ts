083e477ef2e068f39452e9c3368252f15a199a4d
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-02 20:44:28 +0100

    Feat: message sending

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
